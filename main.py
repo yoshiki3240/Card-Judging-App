9bb272eb4df82d2c075131e448d3449e857de381
yoshiki3240 <342064y@gmail.com>
2022-12-01 11:36:55 +0900

    importコードの変更

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request, redirect, render_template, flash
 from werkzeug.utils import secure_filename
-from tensorflow.keras.models import Sequential, load_model
+from tensorflow.python.keras.models import Sequential, load_model
 from tensorflow.keras.preprocessing import image
 
 import numpy as np
@@ -52,4 +52,3 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
     app.run(host ='0.0.0.0',port = port)
 
-    
